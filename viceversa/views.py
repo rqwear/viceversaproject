23f7b82d6a6c129b3a3bb04a3964d47f1a814b1c
rqwear <avatrx@gmail.com>
2021-08-18 23:00:14 +0300

    reverse text in page reverse.html

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    user_text = request.GET['text-to-txet']
+    reverse_text = user_text[::-1]
+    return render(request, 'reverse.html', {'user_text': reverse_text})
